2b4acc5a0c347da7aac8c86e8a765f8415918c72
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-08-23 19:39:04 +0300

    the basics of the menu and footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import Menu from './components/menu/menu';
+import Footer from './components/footer/footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu/>
+        <Footer/>
       </header>
     </div>
   );
