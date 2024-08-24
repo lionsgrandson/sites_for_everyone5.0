f789ffc6972612e73024bbf597c450a516d630b0
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-08-24 17:28:36 +0300

    added all the parts of the nav bar, havn't styled it yet

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,10 +1,32 @@
 import "./menu.css";
+import ILflagImg from "../../img/IlFlagIcon.png";
 export default function Menu() {
   return (
-    <div>
+    <div className="menu-div">
       <h1 className="menu_header">
         S<span>fE</span>
       </h1>
+      <ul className="menu-list">
+        <li>
+          <a href="./about">About</a>
+        </li>
+        <li>
+          <a href="./service">Services</a>
+        </li>
+        <li>
+          <a href="./portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="./blog">Blog</a>
+        </li>
+        <li>
+          <a href="./contact">Contact</a>
+        </li>
+      </ul>
+      <button className="CalltoActionNav">
+        <a>Get Started</a>
+      </button>
+      <img src={ILflagImg} className="ilFlagNav" alt="Israeli Flag Icon"/>
     </div>
   );
 }
