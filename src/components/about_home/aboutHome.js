14a09abd56dd7c2822cb2760e0fbcba19cd9b85b
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-08-27 10:58:57 +0300

    feat(home about): added most of the content for this section

--- src/components/about_home/aboutHome.js
+++ src/components/about_home/aboutHome.js
@@ -1,6 +1,28 @@
-export default function AboutHome(){
-return(
-    <>
-    <h1>About Home</h1></>
-)
+import "./aboutHome.css";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+export default function AboutHome() {
+  return (
+    <div className="home-about-div">
+      <div className="home-about-innder-div">
+        <h1 className="home-about-h1">
+          Hi, I’m Moshe, CEO of Sites for Everyone
+        </h1>
+        <p className="home-about-p">
+          Running a business is hard enough... let us help you take the load
+          off. Our team of experiences developers, designers and Dev-Ops
+          engineers is always ready to provide our client with the most
+          sophisticated final product and the guidance throughout the building
+          process.
+        </p>
+        <button className="home-about-btn">
+          Read More <FaArrowRightLong />
+        </button>
+      </div>
+      <img
+        src=""
+        alt="image of the CEO of the company - Moshe"
+      />
+    </div>
+  );
 }
