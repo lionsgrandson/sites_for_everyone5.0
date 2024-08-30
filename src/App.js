0d3cfcb78025468b0a97619264fd4ac23727b033
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-08-30 20:01:21 +0300

    feat(New Component): crated a component that can be used anywhere there's a block of text and an img

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/hero/hero";
 import AboutHome from "./components/about_home/aboutHome";
 import OurProccess from "./components/OurProccess/OurProccess";
 import PicXtext from "./components/picXtext/picXtext";
+import moseHeadshot from "./img/mosheHeadshot.png";
 
 import "./App.css";
 
@@ -14,9 +15,15 @@ function App() {
         <div className="home-hero">
           <Menu />
           <Hero />
-          <AboutHome />
+          <PicXtext
+            imgXTextImg={moseHeadshot}
+            picXtexth1="Design"
+            textP="Great design starts with you. transform your vision into captivating digital experiences. We craft stunning layouts that not only look great but also connect with your audience."
+            WbtnQ={true}
+            LORImg={false}
+            aboutCls={true}
+          />
           <OurProccess />
-          
         </div>
         {/* <Footer/> */}
       </header>
--- src/components/picXtext/picXtext.js
+++ src/components/picXtext/picXtext.js
@@ -2,16 +2,19 @@ import "./picXtext.css";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function PicXtext(props) {
-    const classNamePr = `home-picXtext-div ${props.LORImg ? "left" : ""}`;
+  const classNamePr = `home-picXtext-div ${props.LORImg ? "left" : ""}`;
+  const aboutClsName = `${props.aboutCls ? "about" : ""}`;
   return (
     // fix the style
     // add switching img left or right
     // button optional
     // TODO deal with the elips
     //TODO fix the text width and hight
-    <div className={classNamePr}>
+    <div className={`${classNamePr} ${props.aboutCls ? "about" : ""}`}>
       <div className="home-picXtext-innder-div">
-        <h1 className="home-picXtext-h1">{props.picXtexth1}</h1>
+        <h1 className={`home-picXtext-h1 ${props.aboutCls ? "about" : ""}`}>
+          {props.picXtexth1}
+        </h1>
         <p className="home-picXtext-p">{props.textP}</p>
         {props.WbtnQ ? (
           <button className="home-picXtext-btn">
