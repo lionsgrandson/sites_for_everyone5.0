22fc796ac13368d16520d7afd2010e6b2eec43bb
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-08-30 17:44:26 +0300

    style(our process): styled the title of the section

--- src/components/OurProccess/OurProccess.js
+++ src/components/OurProccess/OurProccess.js
@@ -2,8 +2,8 @@ import "./OurProccess.css";
 import OurProTitle from "./OurProTitle";
 export default function OurProccess() {
   return (
-    <>
+    <div   className="ourProSection">
       <OurProTitle />
-    </>
+    </ div>
   );
 }
