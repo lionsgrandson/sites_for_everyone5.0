312b4787b7fe2e83d311e1c720254d9a9f3072f7
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-08-31 20:55:27 +0300

    new(services):added teh content and the style of the page

--- src/pages/service.js
+++ src/pages/service.js
@@ -1,9 +1,22 @@
 import Menu from "../components/menu/menu";
 import Footer from "../components/footer/footer";
+import Btn from "../components/btn/btn";
+import "./service.css";
 export default function services() {
   return (
     <>
       <Menu />
+      <div className="servicesDiv">
+        <h1>Your one stop solution for web needs.</h1>
+        <p>
+          from initial design to marketing, we provide al the tools necessary to
+          grow you digital business
+        </p>
+        <Btn
+          btnHref="/contact"
+          bntTxt="Get Started"
+        />
+      </div>
       <Footer />
     </>
   );
