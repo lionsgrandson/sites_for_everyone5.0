40d16ee6c21e911dae2fccc43c8376b3534b72c9
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-08-31 20:44:24 +0300

    new(pages): created pages for the blog and services

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import Menu from "../components/menu/menu";
+import Footer from "../components/footer/footer";
+
+export default function Blog() {
+  return (
+    <>
+      <Menu />
+      <Footer />
+    </>
+  );
+}
---
+++ src/pages/service.js
@@ -0,0 +1,10 @@
+import Menu from "../components/menu/menu";
+import Footer from "../components/footer/footer";
+export default function services() {
+  return (
+    <>
+      <Menu />
+      <Footer />
+    </>
+  );
+}
