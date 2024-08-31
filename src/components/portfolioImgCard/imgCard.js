82f31aeb2389c1f602c021baaf5f3d4cea920060
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-08-31 20:11:12 +0300

    feat(profolio page): Added all the imgs and styled them

---
+++ src/components/portfolioImgCard/imgCard.js
@@ -0,0 +1,13 @@
+import "./imgCard.css";
+export default function ImgCard(props) {
+  return (
+    <div className="imgCardDiv">
+      <img
+        src={props.imgSrc}
+        className="imgCardImg"
+        alt={`A snippit from the ${props.imgTitle} project`}
+      />
+      <h4 className="imgCardTxt">{props.imgTitle}</h4>
+    </div>
+  );
+}
---
+++ src/pages/portfolio.js
@@ -0,0 +1,50 @@
+import Menu from "../components/menu/menu";
+import Footer from "../components/footer/footer";
+import ImgCard from "../components/portfolioImgCard/imgCard";
+import Omnifood from "../img/portfolio/Omnifood-neverCookgain.png";
+import DY from "../img/portfolio/DanielYehuda.jpg";
+import EP from "../img/portfolio/EnterPlus.png";
+import Itai from "../img/portfolio/ItaiArenFeld.png";
+import LFZ from "../img/portfolio/LionsforZion.jpg";
+import Mahogony from "../img/portfolio/MahogonyIsrael.png";
+import Smokin from "../img/portfolio/SmokinT's.jpg";
+import Shalom from "../img/portfolio/ShalomCarmel.jpg";
+import ToraDojo from "../img/portfolio/ToraDojoIsrael.jpg";
+import idfTene1 from "../img/portfolio/idftene.png";
+import idftene2 from "../img/portfolio/idftene2.0.png";
+import Maim from "../img/portfolio/maim-chaim.jpg";
+import "./portoflio.css";
+export default function Portfolio() {
+  const imgArr = [
+    Omnifood,
+    DY,
+    Smokin,
+    Itai,
+    LFZ,
+    Maim,
+    idftene2,
+    Mahogony,
+    idfTene1,
+    EP,
+    Shalom,
+    ToraDojo,
+  ];
+  return (
+    <div>
+      <Menu />
+      <h3 className="protH3">Not convinced yet?</h3>
+      <h1 className="protH1">See Our Work</h1>
+      <div className="imgArrdiv">
+        {imgArr.map((srcImg) => {
+          return (
+            <ImgCard
+              imgSrc={srcImg}
+              imgTitle="OmniFood"
+            />
+          );
+        })}
+      </div>
+      <Footer />
+    </div>
+  );
+}
