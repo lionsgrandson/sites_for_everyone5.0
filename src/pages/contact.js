0a11b7035857ee6e5e9b876b930d2f8c6785c574
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-02 10:06:09 +0300

    style(cta & porfolio & menu & footer): responsive design

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -33,7 +33,6 @@ export default function Contact() {
           }, 1000); // hide message after 1 second
         },
         (error) => {
-          
           setStateMessage(t("Something went wrong, please try again later"));
           setIsSubmitting(false);
           setTimeout(() => {
