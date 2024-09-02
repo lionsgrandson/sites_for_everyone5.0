e8bf77842f2e133947fd57cfea38feb16e7ec7e1
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-02 12:32:33 +0300

    new(thank you page): created a new page that you are sent to after you use the contact page

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -1,7 +1,25 @@
-export default function Thankyou() {
+// ThankYou.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "../components/menu/menu";
+import Footer from "../components/footer/footer";
+import Btn from "../components/btn/btn";
+import "./ThankYou.css";
+
+export default function ThankYou() {
   return (
-    <>
-      <h1>Thank you</h1>
-    </>
+    <div className="thankYouContainer">
+      <Menu />
+      <div className="thankYouContent">
+        <h1 className="thankYouTitle">Thank You!</h1>
+        <p className="thankYouMessage">
+          Your message has been sent successfully.
+        </p>
+        <Link to="/contact">
+          <Btn bntTxt="Go Back" />
+        </Link>
+      </div>
+      <Footer />
+    </div>
   );
 }
