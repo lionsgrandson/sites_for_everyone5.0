9b1bfe419ad6ceb390801f480e02ac7d981cacdc
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-03 14:34:45 +0300

    style(porfolio): responsive design

--- src/components/portfolioImgCard/imgCard.js
+++ src/components/portfolioImgCard/imgCard.js
@@ -1,11 +1,12 @@
 import "./imgCard.css";
+
 export default function ImgCard(props) {
   return (
     <div className="imgCardDiv">
       <img
         src={props.imgSrc}
         className="imgCardImg"
-        alt={`A snippit from the ${props.imgTitle} project`}
+        alt={`A snippet from the ${props.imgTitle} project`}
       />
       <h4 className="imgCardTxt">{props.imgTitle}</h4>
     </div>
