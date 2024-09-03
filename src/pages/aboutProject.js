7eb227587023d399d02e93814ca174a75284b3a0
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-03 17:48:14 +0300

    feat(about project): created the basis for the new page

--- src/pages/aboutProject.js
+++ src/pages/aboutProject.js
@@ -1,1 +1,12 @@
 import "./aboutProject.css";
+import Menu from "../components/menu/menu";
+import Footer from "../components/footer/footer";
+
+export default function AboutProject() {
+  return (
+    <div>
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
