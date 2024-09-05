e5f0f6354920dd123c309633f5f3d463e86cc89b
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-05 11:53:00 +0300

    feat(servies): got the translation template ready

--- src/pages/service.js
+++ src/pages/service.js
@@ -7,14 +7,15 @@ export default function services() {
     <>
       <Menu />
       <div className="servicesDiv">
-        <h1>Your one stop solution for web needs.</h1>
+        <h1>{t("Your one stop solution for web needs")}.</h1>
         <p>
-          from initial design to marketing, we provide al the tools necessary to
-          grow you digital business
+          {t(
+            "from initial design to marketing, we provide al the tools necessary to grow you digital business"
+          )}
         </p>
         <Btn
           btnHref="/contact"
-          bntTxt="Get Started"
+          bntTxt={t("Get Started")}
         />
       </div>
       <Footer />
