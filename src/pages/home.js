4e36a8f9e4bcb9afd314bdc1149ef84412db52b8
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-05 11:07:05 +0300

    feat(home): added the format for the translation text

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,8 +7,9 @@ import moseHeadshot from "../img/mosheHeadshot.png";
 import Projects from "../components/projects/projects";
 import CTA from "../components/cta/cta";
 import "./home.css";
-
+import { useTranslation } from "react-i18next";
 function Home() {
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
@@ -17,8 +18,10 @@ function Home() {
           <Hero />
           <PicXtext
             imgXTextImg={moseHeadshot}
-            picXtexth1="Hi, I’m Moshe, CEO of Sites for Everyone"
-            textP="Running a business is hard enough... let us help you take the load off. Our team of experiences developers, designers and Dev-Ops engineers is always ready to provide our client with the most sophisticated final product and the guidance throughout the building process.  "
+            picXtexth1={t("Hi, I’m Moshe, CEO of Sites for Everyone")}
+            textP={t(
+              "Running a business is hard enough... let us help you take the load off. Our team of experiences developers, designers and Dev-Ops engineers is always ready to provide our client with the most sophisticated final product and the guidance throughout the building process."
+            )}
             WbtnQ={true}
             LORImg={false}
             aboutCls={true}
@@ -26,15 +29,16 @@ function Home() {
           <OurProccess />
         </div>
         <Projects
-          h2Proj="Results matter."
-          h1Proj="See What We Mean"
+          h2Proj={t("Results matter.")}
+          h1Proj={t("See What We Mean")}
           exitNum="4"
         />
         <CTA
-          ctaH1="Ready to Start
-Your Jorney?"
-          ctaP="Our expert team is eager to collaborate with you on creating a stunning, high-performing website. Whether you need a fresh design, improved functionality, or expert digital marketing strategies, we've got you covered. Contact us today for a free consultation and let's bring your vision to life"
-          ctaBtn="Contact Us"
+          ctaH1={t("Ready to Start Your Jorney?")}
+          ctaP={t(
+            "Our expert team is eager to collaborate with you on creating a stunning, high-performing website. Whether you need a fresh design, improved functionality, or expert digital marketing strategies, we've got you covered. Contact us today for a free consultation and let's bring your vision to life"
+          )}
+          ctaBtn={t("Contact Us")}
           btnOnBtn={true}
         />
         <Footer />
