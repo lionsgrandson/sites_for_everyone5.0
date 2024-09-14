90b62685f6c25f8cc3e689e1a29a50371f028542
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-14 13:41:29 +0300

    feat(tora dojo): added the translation

--- src/pages/projects/ToraDojo.js
+++ src/pages/projects/ToraDojo.js
@@ -2,19 +2,25 @@ import AboutProject from "../../components/projDetials/aboutProject";
 import projLogo from "../../img/toraDojo/toradojoLogo.png";
 import screen1 from "../../img/portfolio/ToraDojoIsrael.jpg";
 import screen2 from "../../img/toraDojo/screen2.png";
+import { useTranslation } from "react-i18next";
+
 export default function ToraDojo() {
+  const { t } = useTranslation();
+
   return (
     <div>
       <AboutProject
-        projName="Tora Dojo Israel"
-        client="Tora Dojo Israel"
+        projName={t("Tora Dojo Israel")}
+        client={t("Tora Dojo Israel")}
         year="2017"
-        services="Web Development, Maintinance"
+        services={t("Web Development, Maintinance")}
         linkHref="https://toradojo.co.il"
         projimg1={screen1}
         projimg2={screen2}
         projLogo={projLogo}
-        projText="This is the largest Tora Dojo branch outside the United States, founded by sixth-degree black belt Arthur Gribetz, along with a dedicated team of high-ranking expert instructors."
+        projText={t(
+          "This is the largest Tora Dojo branch outside the United States, founded by sixth-degree black belt Arthur Gribetz, along with a dedicated team of high-ranking expert instructors."
+        )}
       />
     </div>
   );
