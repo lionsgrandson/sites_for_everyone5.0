9717d3cca1886ab405fb96ce3f466b2618d08d4b
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-14 13:18:46 +0300

    feat(enter plus): added the translation

--- src/pages/projects/enterPlus.js
+++ src/pages/projects/enterPlus.js
@@ -2,19 +2,25 @@ import AboutProject from "../../components/projDetials/aboutProject";
 import projLogo from "../../img/enterplus/enterplusLogo.png";
 import screen1 from "../../img/portfolio/EnterPlus.png";
 import screen2 from "../../img/enterplus/screen2.png";
+import { useTranslation } from "react-i18next";
+
 export default function EnterPlus() {
+  const { t } = useTranslation();
+
   return (
     <div>
       <AboutProject
-        projName="Enter Plus"
-        client="Enter Plus"
+        projName={t("Enter Plus")}
+        client={t("Enter Plus")}
         year="2017"
-        services="Web Development, SEO, Maintinance"
+        services={t("Web Development, SEO, Maintinance")}
         linkHref="https://enterplusco.com"
         projimg1={screen1}
         projimg2={screen2}
         projLogo={projLogo}
-        projText="Enter Plus has created a life-saving bracelet that detects drowning by monitoring vital signs. When a drowning incident occurs, it automatically alerts lifeguards, parents, and emergency services like 911. This innovative device enhances pool safety and provides peace of mind for families."
+        projText={t(
+          "Enter Plus has created a life-saving bracelet that detects drowning by monitoring vital signs. When a drowning incident occurs, it automatically alerts lifeguards, parents, and emergency services like 911. This innovative device enhances pool safety and provides peace of mind for families."
+        )}
       />
     </div>
   );
