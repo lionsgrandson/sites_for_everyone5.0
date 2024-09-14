4fe2b1d867843715543524f5b59c1d19a5810db7
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-14 13:28:34 +0300

    feat(daniel yehuda): added the translation

--- src/pages/projects/danielYehuda.js
+++ src/pages/projects/danielYehuda.js
@@ -3,19 +3,25 @@ import AboutProject from "../../components/projDetials/aboutProject";
 import projLogo from "../../img/danielYehuda/dyLogo.png";
 import OmnifoodScreen1 from "../../img/portfolio/DanielYehuda.jpg";
 import OmnifoodScreen2 from "../../img/danielYehuda/DanielYehuda2.png";
+import { useTranslation } from "react-i18next";
+
 export default function DanielYehuda() {
+  const { t } = useTranslation();
+
   return (
     <div>
       <AboutProject
-        projName="Daniel Yehuda"
-        client="Daniel Yehuda"
+        projName={t("Daniel Yehuda")}
+        client={t("Daniel Yehuda")}
         year="2020"
-        services="Design, Web Development, maintenance, SEO"
+        services={t("Design, Web Development, maintenance, SEO")}
         linkHref="https://www.danielyehuda.co.il/"
         projimg1={OmnifoodScreen1}
         projimg2={OmnifoodScreen2}
         projLogo={projLogo}
-        projText="Daniel Yehuda is a Jewish musician who connects young Jewish souls through uplifting music. He offers customized performances, workshops, and events worldwide, focusing on Judaism and Zionism. Daniel also serves as a cantor and provides entertainment for celebrations like weddings and bar mitzvahs."
+        projText={t(
+          "Daniel Yehuda is a Jewish musician who connects young Jewish souls through uplifting music. He offers customized performances, workshops, and events worldwide, focusing on Judaism and Zionism. Daniel also serves as a cantor and provides entertainment for celebrations like weddings and bar mitzvahs."
+        )}
       />
     </div>
   );
