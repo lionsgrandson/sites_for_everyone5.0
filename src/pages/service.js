c386337f58f7a89dcedef64d6fa69fe8617cd971
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-09-15 03:46:13 +0300

    style(HERO SERVICE): added the background img and styled the text

--- src/pages/service.js
+++ src/pages/service.js
@@ -4,23 +4,28 @@ import Btn from "../components/btn/btn";
 import "./service.css";
 import { useTranslation } from "react-i18next";
 import PicXtext from "../components/picXtext/picXtext";
+import heroimg from "../img/service-Hero.png";
 import CTA from "../components/cta/cta";
 export default function Services() {
   const { t } = useTranslation();
   return (
     <>
-      <Menu />
-      <div className="servicesDiv">
-        <h1>{t("Your one stop solution for web needs")}.</h1>
-        <p>
-          {t(
-            "from initial design to marketing, we provide al the tools necessary to grow you digital business"
-          )}
-        </p>
-        <Btn
-          btnHref="/contact"
-          bntTxt={t("Get Started")}
-        />
+      <div className="heroSection">
+        <Menu />
+        <div className="servicesDiv">
+          <h1>{t("Your one stop solution for web needs")}.</h1>
+          <p>
+            {t(
+              "from initial design to marketing, we provide al the tools necessary to grow you digital business"
+            )}
+          </p>
+          <div className="btn-div">
+            <Btn
+              btnHref="/contact"
+              bntTxt={t("Get Started")}
+            />
+          </div>
+        </div>
       </div>
       <div></div>
       <CTA
