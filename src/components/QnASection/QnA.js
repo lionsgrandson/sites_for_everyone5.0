18f1de7ed5d3c190658cd2cbd10e0b3aace06c93
moshe schwartzberg <mosheschwartzberg@gmail.com>
2024-11-13 16:32:11 +0200

    style(QnA): styled the QnA to match the figma

--- src/components/QnASection/QnA.js
+++ src/components/QnASection/QnA.js
@@ -1,13 +1,14 @@
 import Btn from "../btn/btn";
 import { useTranslation } from "react-i18next";
 import QnALis from "./QnAList";
+import "./QnA.css";
 
 export default function QnA() {
   const { t } = useTranslation();
 
   return (
-    <div>
-      <h1>{t("Questions")}?</h1>
+    <div className="divQnA">
+      <h1 className="h1QnA">{t("Questions")}?</h1>
       <QnALis />
       <Btn
         bntTxt={t("Get a quote")}
--- src/components/QnASection/QnAList.js
+++ src/components/QnASection/QnAList.js
@@ -1,17 +1,37 @@
 import { useTranslation } from "react-i18next";
-
+import "./QnA.css";
+import { useState } from "react";
 export default function QnALis() {
   const { t } = useTranslation();
 
   return (
     <div>
-      <ul>
-        <li>{t("What is the price of a website?")}</li>
-        <li>{t("How long does the process take before the site is live?")}</li>
-        <li>
-          {t(
-            "Will there be support and maintenance available even after the project is over?"
-          )}
+      <ul className="ulQnA">
+        <li className="liQnA">
+          <a
+            href="#"
+            className="aQnA"
+          >
+            {t("What is the price of a website?")}{" "}
+          </a>
+        </li>
+        <li className="liQnA">
+          <a
+            href="#"
+            className="aQnA"
+          >
+            {t("How long does the process take before the site is live?")}
+          </a>
+        </li>
+        <li className="liQnA">
+          <a
+            href="#"
+            className="aQnA"
+          >
+            {t(
+              "Will there be support and maintenance available even after the project is over?"
+            )}
+          </a>
         </li>
       </ul>
     </div>
