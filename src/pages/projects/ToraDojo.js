5f109a85b75ae023ad57a4ed6ed6f3228ad68ea9
moshe schwartzberg <mosheschwartzberg@gmail.com>
2025-04-24 20:42:33 +0300

    SEO(Tora Dojo): added SEO meta tags

--- src/pages/projects/ToraDojo.js
+++ src/pages/projects/ToraDojo.js
@@ -9,6 +9,63 @@ export default function ToraDojo() {
 
   return (
     <div>
+      <Helmet>
+        <title>Tora Dojo Israel | Moshe Schwartzberg Portfolio</title>
+        <meta
+          name="description"
+          content="Tora Dojo Israel’s martial arts site by Moshe Schwartzberg, built for global training excellence."
+        />
+        <meta
+          name="keywords"
+          content="martial arts website, custom web development, Moshe Schwartzberg, Tora Dojo Israel"
+        />
+        <meta
+          property="og:title"
+          content="Tora Dojo Israel | Moshe Schwartzberg Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Explore Tora Dojo Israel, a custom martial arts site by Moshe Schwartzberg for expert training."
+        />
+        <meta
+          property="og:image"
+          content="https://mosheschwartzberg.com/logo192.png"
+        />
+        <meta
+          property="og:url"
+          content="https://mosheschwartzberg.com/projects/tora-dojo"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Tora Dojo Israel | Moshe Schwartzberg Portfolio"
+        />
+        <meta
+          name="twitter:description"
+          content="Explore Tora Dojo Israel, a custom martial arts site by Moshe Schwartzberg for expert training."
+        />
+        <meta
+          name="twitter:image"
+          content="https://mosheschwartzberg.com/logo192.png"
+        />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "CreativeWork",
+            name: "Tora Dojo Israel",
+            description:
+              "Tora Dojo Israel is the largest Tora Dojo branch outside the United States, founded by sixth-degree black belt Arthur Gribetz, offering expert martial arts training.",
+            url: "https://toradojo.co.il",
+            creator: {
+              "@type": "Person",
+              name: "Moshe Schwartzberg",
+              email: "mosheschwartzberg@gmail.com",
+              telephone: "+972587076077",
+            },
+          })}
+        </script>
+      </Helmet>
       <AboutProject
         projName={t("Tora Dojo Israel")}
         client={t("Tora Dojo Israel")}
