3b0fb1ba96d25c99b20b63c0db74797fdbeb4bc3
moshe schwartzberg <mosheschwartzberg@gmail.com>
2025-04-24 20:38:23 +0300

    SEO(Maim): added SEO meta tags

--- src/pages/projects/maim.js
+++ src/pages/projects/maim.js
@@ -9,6 +9,63 @@ export default function LFZ() {
 
   return (
     <div>
+      <Helmet>
+        <title>Maim Chaim | Moshe Schwartzberg Portfolio</title>
+        <meta
+          name="description"
+          content="Maim Chaim’s Torah school site by Moshe Schwartzberg, blending education and faith."
+        />
+        <meta
+          name="keywords"
+          content="Torah school website, custom web development, Moshe Schwartzberg, religious education site"
+        />
+        <meta
+          property="og:title"
+          content="Maim Chaim | Moshe Schwartzberg Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Maim Chaim, a Torah school site by Moshe Schwartzberg, fosters joyful learning."
+        />
+        <meta
+          property="og:image"
+          content="https://mosheschwartzberg.com/logo192.png"
+        />
+        <meta
+          property="og:url"
+          content="https://mosheschwartzberg.com/projects/maim-chaim"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Maim Chaim | Moshe Schwartzberg Portfolio"
+        />
+        <meta
+          name="twitter:description"
+          content="Maim Chaim, a Torah school site by Moshe Schwartzberg, fosters joyful learning."
+        />
+        <meta
+          name="twitter:image"
+          content="https://mosheschwartzberg.com/logo192.png"
+        />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "CreativeWork",
+            name: "Maim Chaim",
+            description:
+              "Maim Chaim is a Torah-based school in Jerusalem providing holistic education for boys and girls, combining religious studies with core subjects.",
+            url: "https://maimchaim.org.il/",
+            creator: {
+              "@type": "Person",
+              name: "Moshe Schwartzberg",
+              email: "mosheschwartzberg@gmail.com",
+              telephone: "+972587076077",
+            },
+          })}
+        </script>
+      </Helmet>
       <AboutProject
         projName={t("Maim Chaim")}
         client={t("Maim Chaim")}
