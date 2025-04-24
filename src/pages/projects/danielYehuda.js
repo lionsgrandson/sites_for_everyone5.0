0b514fadbf9be96f2436d1e7c4ec6b3e7cbeab0e
moshe schwartzberg <mosheschwartzberg@gmail.com>
2025-04-24 20:36:08 +0300

    SEO(Daniel Yehuda): added SEO meta tags

--- src/pages/projects/danielYehuda.js
+++ src/pages/projects/danielYehuda.js
@@ -10,6 +10,63 @@ export default function DanielYehuda() {
 
   return (
     <div>
+      <Helmet>
+        <title>Daniel Yehuda | Moshe Schwartzberg Portfolio</title>
+        <meta
+          name="description"
+          content="Daniel Yehuda’s site by Moshe Schwartzberg connects Jewish youth through music and events."
+        />
+        <meta
+          name="keywords"
+          content="Jewish music website, custom web development, Moshe Schwartzberg, event performance site"
+        />
+        <meta
+          property="og:title"
+          content="Daniel Yehuda | Moshe Schwartzberg Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Explore Daniel Yehuda’s music site, a custom project by Moshe Schwartzberg for Jewish events."
+        />
+        <meta
+          property="og:image"
+          content="https://mosheschwartzberg.com/logo192.png"
+        />
+        <meta
+          property="og:url"
+          content="https://mosheschwartzberg.com/projects/daniel-yehuda"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Daniel Yehuda | Moshe Schwartzberg Portfolio"
+        />
+        <meta
+          name="twitter:description"
+          content="Explore Daniel Yehuda’s music site, a custom project by Moshe Schwartzberg for Jewish events."
+        />
+        <meta
+          name="twitter:image"
+          content="https://mosheschwartzberg.com/logo192.png"
+        />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "CreativeWork",
+            name: "Daniel Yehuda",
+            description:
+              "Daniel Yehuda is a Jewish musician who connects young Jewish souls through uplifting music, offering customized performances and workshops worldwide.",
+            url: "https://www.danielyehuda.co.il/",
+            creator: {
+              "@type": "Person",
+              name: "Moshe Schwartzberg",
+              email: "mosheschwartzberg@gmail.com",
+              telephone: "+972587076077",
+            },
+          })}
+        </script>
+      </Helmet>
       <AboutProject
         projName={t("Daniel Yehuda")}
         client={t("Daniel Yehuda")}
