abdb1face7f9bb99aea99cdac8df1181e108265f
moshe schwartzberg <mosheschwartzberg@gmail.com>
2025-05-19 18:54:28 +0300

    style(GReviwes): fixed the width to fit mobile

--- src/components/gReviwes/GReviews.js
+++ src/components/gReviwes/GReviews.js
@@ -1,13 +1,18 @@
 import { ReactGoogleReviews } from "react-google-reviews";
 import "react-google-reviews/dist/index.css";
-
+import "./GReviews.css";
 function GReviews() {
   const featurableWidgetId = "a534fb67-9511-4753-806f-671b133a6f72";
 
+  <h1 className="">Top Reviews</h1>;
   return (
     <>
-      <h1 className="">Top Reviews</h1>
-      <ReactGoogleReviews layout="carousel" featurableId={featurableWidgetId} />
+      <div className="revDiv">
+        <ReactGoogleReviews
+          layout="carousel"
+          featurableId={featurableWidgetId}
+        />
+      </div>
     </>
   );
 }
